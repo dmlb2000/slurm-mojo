7884e20ef49d42a1877ba31297331db96cd8d04c
root <root@slurm-mojo.emsl.pnl.gov>
2014-08-21 23:52:19 -0400

    this does quite a bit now submit delete query

--- lib/Slurm/Mojo.pm
+++ lib/Slurm/Mojo.pm
@@ -2,4 +2,22 @@ package Slurm::Mojo;
 
 our $VERSION = '0.01';
 
+use Authen::Krb5::Simple;
+
+sub AuthRequest {
+  my $c = shift;
+  my $json = JSON->new->allow_nonref;
+  my $httpdata = $json->allow_blessed->convert_blessed->decode( $c->req->body );
+  die "need to pass username" unless exists $httpdata->{"username"};
+  die "need to pass password" unless exists $httpdata->{"password"};
+  my $user = $httpdata->{"username"};
+  my $pass = $httpdata->{"password"};
+  my $krb = Authen::Krb5::Simple->new();
+  my $authen = $krb->authenticate($user, $pass);
+  unless($authen) {
+    my $errmsg = $krb->errstr();
+    die "User: $user authentication failed: $errmsg\n";
+  }
+  return getpwnam($user);
+};
 
